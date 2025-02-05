66cc11ab65391696be435e82fa07c2cf7917b2af
Mikhail Lapshin <urbancell022@gmail.com>
2025-02-05 17:56:58 +0700

    First commit

---
+++ src/seminar01/HW01/Calculator.java
@@ -0,0 +1,47 @@
+package seminar01.HW01;
+
+public class Calculator {
+    public static int calculation(int firstOperand, int secondOperand, char operator) {
+        int result;
+
+        switch (operator) {
+            case '+':
+                result = firstOperand + secondOperand;
+                break;
+            case '-':
+                result = firstOperand - secondOperand;
+                break;
+            case '*':
+                result = firstOperand * secondOperand;
+                break;
+            case '/':
+                if (secondOperand != 0) {
+                    result = firstOperand / secondOperand;
+                    break;
+                } else {
+                    throw new ArithmeticException("Division by zero is not possible");
+                }
+            default:
+                throw new IllegalStateException("Unexpected value operator: " + operator);
+        }
+        return result;
+    }
+
+    public static double squareRootExtraction(double num) {
+        if (num < 0) {
+            throw new IllegalArgumentException("Cannot calculate square root of a negative number");
+        }
+        return Math.sqrt(num);
+    }
+
+    public static double calculatingDiscount(double purchaseAmount, int discountAmount) {
+        if (purchaseAmount < 0) {
+            throw new IllegalArgumentException("The purchase amount cannot be negative.");
+        }
+        if (discountAmount < 0 || discountAmount > 100) {
+            throw new IllegalArgumentException("The discount amount must be between 0 and 100");
+        }
+        double discount = (discountAmount / 100.0) * purchaseAmount;
+        return purchaseAmount;
+    }
+}
